3c859d4a808bfb8062a3e6165a6d89ce336e86a2
emily-zhangg <emily@officecleanz.com>
2021-09-25 01:57:48 -0700

    working code

---
+++ src/__tests__/uploadFile.spec.js
@@ -0,0 +1,8 @@
+describe('Filter function', () => {
+  test('it should filter by a search term (link)', () => {
+    expect(1 + 2).toBe(3);
+  });
+  test('it should filter by a search term (link)', () => {
+    expect(true).toBe(true);
+  });
+});
